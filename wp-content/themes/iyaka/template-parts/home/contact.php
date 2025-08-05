524f9128cb3faecafa09bc1387345172e44fe7cd
Gwenbatta0704 <gwenaellebatta@gmail.com>
2025-08-05 21:01:22 +0200

    Made contact

--- wp-content/themes/iyaka/template-parts/home/contact.php
+++ wp-content/themes/iyaka/template-parts/home/contact.php
@@ -1,1 +1,115 @@
 <?php
+
+$title = get_field('contact_title');
+$word = get_field('contact_word');
+
+$informations = get_field('iyaka_informations', 5);
+$legalName = get_the_title(5);
+$phone = $informations['contact_phone'];
+$email = $informations ["contact_mail"];
+$distance = $informations ["contact_distance"];
+$schedules = $informations ["contact_schedules"];
+
+$facebook = $informations ["contact_facebook"];
+$instagram = $informations ["contact_instagram"];
+$linkedin = $informations ["contact_linkedin"];
+?>
+<section class="pb-20 rl:pb-40 pt-24">
+<!--    <h2 class="text-brand-light font-quicksand font-bold text-4xl md:text-5xl lg:text-6xl xg:text-6.5xl relative z-1  rg:!leading-snug rl:max-w-[90%] 3xl:max-w-[70%] "-->
+<!--        id="contact">--><?php //= $title ?><!--</h2>-->
+
+    <div class="grid-default-12 rl:gap-2  gap-1 text-center relative mb-20 md:mb-40">
+        <h2 class="text-brand-light font-quicksand font-bold text-4xl md:text-5xl lg:text-6xl xg:text-7xl relative z-1 col-span-3 col-start-2 max-md:text-end md:col-span-4 md:col-start-4 rl:col-start-7 rl:col-span-4">
+            <?= $title?>
+        </h2>
+        <span class="inline-block w-[70vw] md:w-[65vw] absolute bg-brand left-0 md:-left-20 h-20 -top-5 rg:h-32 xg:h-40 rg:-top-10"></span>
+    </div>
+    <div class="grid-default-12 relative max-md:gap-y-20">
+        <section class="col-span-full md:col-span-4 rl:col-span-5 lg:col-span-4 row-span-2 rl:row-span-2 ">
+            <h3 class="tracking-widest uppercase font-quicksand text-brand-middle/75 xl:text-5xl rl:text-4xl md:text-3xl text-2xl mt-4 first:mt-0 font-bold md:!leading-snug"><?= __('Informations', 'iyaka') ?></h3>
+            <ul class="!m-0 divide-brand-light xl:text-xl flex flex-col text-lg divide-y">
+                <?php if ($phone) : ?>
+                    <li>
+                        <a href="tel:<?= filter_var(str_replace(['-', '(0)'], '', $phone), FILTER_SANITIZE_NUMBER_INT) ?>"
+                           target="_blank"
+                           class="underlined-link after:bg-brand fill-brand ease-all rl:py-4 flex items-center w-full gap-4 py-3 text-grey">
+                            <svg aria-hidden="true" class="shrink-0 fill-brand-middle ease-all w-5 h-5">
+                                <use xlink:href="#phone"></use>
+                            </svg>
+                            <span><?= $phone; ?></span>
+                        </a>
+                    </li>
+                <?php endif; ?>
+                <?php if ($email) : ?>
+                    <li>
+                        <a href="mailto:<?= $email ?>" target="_blank"
+                           class="underlined-link after:bg-brand fill-brand ease-all rl:py-4 flex items-center w-full gap-4 py-3 !text-grey">
+                            <svg aria-hidden="true" class="shrink-0 fill-brand-middle ease-all w-5 h-5">
+                                <use xlink:href="#mail"></use>
+                            </svg>
+                            <span><?= $email; ?></span>
+                        </a>
+                    </li>
+                <?php endif; ?>
+                <?php if ($distance) : ?>
+                    <li class="fill-brand-middle  ease-all rl:py-4 relative flex w-full gap-4 py-3 text-grey">
+                        <svg aria-hidden="true" class="shrink-0 ease-all w-5 h-5 mt-1.5">
+                            <use xlink:href="#car"></use>
+                        </svg>
+                        <span><?= $distance; ?></span>
+                    </li>
+                <?php endif; ?>
+                <?php if ($schedules) : ?>
+                    <li class="fill-brand-middle ease-all rl:py-4 relative flex  w-full gap-4 py-3 text-grey">
+                        <svg aria-hidden="true" class="shrink-0 ease-all w-5 h-5 mt-1.5">
+                            <use xlink:href="#schedules"></use>
+                        </svg>
+                        <span><?= $schedules; ?></span>
+
+                    </li>
+                <?php endif; ?>
+            </ul>
+        </section>
+        <section class="col-span-full md:col-span-3 md:col-start-6 rl:col-span-5 rl:col-start-7">
+            <h3 class="tracking-widest uppercase font-quicksand text-brand-middle/75 xl:text-5xl rl:text-4xl md:text-3xl text-2xl mt-4 first:mt-0 font-bold md:!leading-snug"><?= __('Réseaux sociaux', 'iyaka') ?></h3>
+            <ul class="socials-menu mt-5 rl:mt-10 !h-fit">
+                <?php if ($facebook) : ?>
+                    <li>
+                        <a href="<?= $facebook ?>" target="_blank" class="group">
+                            <span class="sr-only"><?= __('Voir la page Facebook', 'iyaka') ?></span>
+                            <svg class="fill-brand-light group-hover:fill-brand-dark group-focus-visible:fill-brand-dark group-hover:scale-110 ease-all w-4 h-4 rl:w-6 rl:h-6">
+                                <use xlink:href="#facebook"></use>
+                            </svg>
+                        </a>
+                    </li>
+                <?php endif; ?>
+                <?php if ($instagram) : ?>
+                    <li>
+                        <a href="<?= $instagram ?>" target="_blank" class="group">
+                            <span class="sr-only"><?= __('Voir la page Instagram', 'iyaka') ?></span>
+                            <svg class="fill-brand-light group-hover:fill-brand-dark group-focus-visible:fill-brand-dark group-hover:scale-110 ease-all w-4 h-4 rl:w-6 rl:h-6">
+                                <use xlink:href="#instagram"></use>
+                            </svg>
+                        </a>
+                    </li>
+                <?php endif; ?>
+                <?php if ($linkedin) : ?>
+                    <li>
+                        <a href="<?= $linkedin ?>" target="_blank" class="group">
+                            <span class="sr-only"><?= __('Voir le profil Linkedin', 'iyaka') ?></span>
+                            <svg class="fill-brand-light group-hover:fill-brand-dark group-focus-visible:fill-brand-dark group-hover:scale-110 ease-all w-4 h-4 rl:w-6 rl:h-6">
+                                <use xlink:href="#linkedin"></use>
+                            </svg>
+                        </a>
+                    </li>
+                <?php endif; ?>
+            </ul>
+        </section>
+
+    <p class=" relative z-1 uppercase font-quicksand  text-brand-light/30 text-6xl rl:text-7xl lg:text-8xl font-semibold   text-end max-md:-translate-x-14 md:max-rl:-translate-x-20 3xl:-translate-x-28 col-span-4  rl:col-span-4 rl:col-end-13  h-fit self-end"><?= $word ?></p>
+    <span class="inline-block  w-[50vw] sm:w-[25vw] md:w-[25vw] rg:w-[20vw] xg:w-[15vw] absolute bg-brand right-0 h-20  rg:h-32 xg:h-40 bottom-0"></span>
+
+
+    </div>
+
+</section>
--- wp-content/themes/iyaka/template-parts/home/cta.php
+++ wp-content/themes/iyaka/template-parts/home/cta.php
@@ -1,1 +1,29 @@
 <?php
+$title = get_field('cta_title');
+$content = get_field('cta_content');
+$word = get_field('cta_word');
+$img = get_field('cta_img');
+$button = get_field('cta_button');
+?>
+
+<section aria-labelledby="cta" class="grid-default-12 max-rg:items-center max-rg:gap-12 max-rg:!flex max-rg:flex-col-reverse bg-brand/65 relative pt-12 pb-24 mt-40 mb-32">
+    <div class="col-span-full rg:col-start-3 rg:col-span-6 rl:col-start-3 rl:col-span-10 lg:col-start-4 lg:col-span-9 xl:pl-10 h-full">
+
+
+        <h2 class="text-brand-light font-quicksand font-bold text-4xl md:text-5xl lg:text-6xl xg:text-6.5xl relative z-1 mb-6 rg:!leading-snug rl:max-w-[90%] 3xl:max-w-[70%]" id="cta"></h2>
+        <div class="flex max-rl:flex-col max-3xl:justify-between 3xl:gap-40 max-rl:gap-14 items-center rl:items-end">
+            <div class="text-content  rl:leading-10 rl:max-w-[650px]">
+                <?= $content ?>
+            </div>
+            <a class="btn-primary !mt-0 text-nowrap max-md:w-full text-center rl:self-end" href="<?= $button['url'] ?>">
+                <span class="relative z-10"><?= $button['title'] ?></span>
+            </a>
+        </div>
+    </div>
+
+
+    <div class="-mt-32 md:max-rg:-mt-40 outline outline-[12px] lg:outline-[20px] rg:absolute outline-brand-middle rounded-full aspect-square w-[60%] h-[60%] sm:w-[50%] sm:h-[50%] rg:w-[380px] rg:h-[380px] lg:w-[450px] lg:h-[450px] xg:w-[520px] xg:h-[520px] 3xl:w-[620px] 3xl:h-[620px] overflow-hidden rg:top-20 rg:-left-28 rl:-left-32 rl:top-20 lg:-left-24 lg:top-20 xg:-left-[7%] xg:top-[20%] 3xl:top-[5%] 3xl:-left-[5%]">
+        <?php echo wp_get_attachment_image($img['id'], '1536x1536', false, ['class' => 'ease-all w-full h-full  inline-block  ']); ?>
+    </div>
+<p class="max-md:hidden absolute uppercase font-quicksand right-0 -bottom-8 lg:-bottom-12 text-brand-light/30 text-6xl rl:text-7xl lg:text-8xl font-semibold "><?= $word ?></p>
+</section>
